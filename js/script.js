d88fb63ee3a164483fe8efa0af860e242f844191
Maliko News <balamine129@gmail.com>
2024-03-12 01:58:47 +0000

    design

--- js/script.js
+++ js/script.js
@@ -1,33 +1,72 @@
+const affiche=document.getElementById('groupe');
+const input=document.getElementById('textValue');
+const API='./js/database.json';
+let EDM=[];
 
-// let EDM=[];
-let test=[];
-fetch(
-    './js/database.json'
-)
+
+const  getData = async ()=>{
+
+fetch(API)
 .then((res) => {
     return res.json();
 })
 .then((prog) => {
-
-          test = prog.EDM.filter((data) => {
-            let b = data;
-           
-            // console.log(b);
-        
-    // EDM = prog.EDM.map((prog) => {
-
-    //     const code = {
-    //         groupe: prog.groupe,
-    //         quartier:prog.quartier,
-    //         programme: prog.programme,
-    //     };
-    //    console.log(code);
-
-
-
+    let query = input.value;
+    //console.log("mot: ", query);
+    let edm = prog.EDM.filter((evenData)=>{
+        if (query===" ") {return evenData }
+         else if(evenData.quartier.toLowerCase().includes(query.toLowerCase())) {
+          return evenData;
+        } 
     })
+    .map((data) => {
+        const {groupe,quartier,programme,link,heures,dates} =data;
+        return`
+        <div class="col-lg-6">
+        <div class="box">
+         <div class="job_content-box" >
+            <div class="img-box">
+            <img src="images/edm.jpeg" alt="">
+            </div>
+        <div class="detail-box">
+          <h5>
+            ${groupe}
+          </h5>
+          <div class="detail-info">
+            <h6>
+              <span>
+                ${dates}
+              </span>
+            </h6>
+            <h6>
+              
+              <span>
+                 ${heures} 
+              </span>
+            </h6>
+          </div>
+        </div>
+      </div>
+      <div class="option-box">
+        <a href="${link}" class="apply-btn">
+          Savoir plus
+        </a>
+      </div>
+      </div>
+      
+      </div>
+        `
+    }).join("")
+    affiche.innerHTML=edm;
 
 })
+
+}
+getData();
+input.addEventListener("input", ()=>{
+    getData();
+})
+
 
 
 
