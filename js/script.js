22d6c62a7f9ef6a0d2733afa606483a7dc656ab8
Maliko News <balamine129@gmail.com>
2024-03-12 11:32:11 +0000

    icon and image

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const affiche=document.getElementById('groupe');
-const input=document.getElementById('textValue');
+const input=document.getElementById('mots');
 const API='./js/database.json';
 let EDM=[];
 
@@ -14,7 +14,8 @@ fetch(API)
     let query = input.value;
     //console.log("mot: ", query);
     let edm = prog.EDM.filter((evenData)=>{
-        if (query===" ") {return evenData }
+        if (query===" ") {
+          return evenData }
          else if(evenData.quartier.toLowerCase().includes(query.toLowerCase())) {
           return evenData;
         } 
@@ -24,11 +25,11 @@ fetch(API)
         return`
         <div class="col-lg-6">
         <div class="box">
-         <div class="job_content-box" >
-            <div class="img-box">
-            <img src="images/edm.jpeg" alt="">
-            </div>
-        <div class="detail-box">
+            <div class="job_content-box" >
+                <div class="img-box">
+                <img src="images/edm.jpeg" alt="">
+                </div>
+            <div class="detail-box">
           <h5>
             ${groupe}
           </h5>
@@ -45,8 +46,11 @@ fetch(API)
               </span>
             </h6>
           </div>
-        </div>
-      </div>
+        </div>          
+
+      </div>        
+      <p id="quart" style="font-size:13px; margin-top:15px;">${quartier}
+          </p>
       <div class="option-box">
         <a href="${link}" class="apply-btn">
           Savoir plus
@@ -66,6 +70,27 @@ getData();
 input.addEventListener("input", ()=>{
     getData();
 })
+function traiter()
+        {
+        var leTexte = document.getElementById("groupe").innerHTML;
+        var termes = document.getElementById("mots").value;
+
+        leTexte = leTexte.split("<span style=\"color:white; background-color:#e41a2b;\">").join("");
+        leTexte = leTexte.split("</span>").join("");
+       
+       
+
+        if(termes!="")
+        {
+        var expReg = new RegExp(termes, "ig");
+        var formeRplcmt = "<span style='color:white; background-color:#e41a2b;'>" + termes +"</span>";
+         
+        leTexte = leTexte.replace(expReg, formeRplcmt);
+        document.getElementById("groupe").innerHTML = leTexte;
+       
+        
+        }
+        }
 
 
 
